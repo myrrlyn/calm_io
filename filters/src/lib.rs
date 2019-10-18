8e30efc62a489c5a35890bee19faf338acfb9d47
Atul Bhosale <atul1bhosale@gmail.com>
2019-10-18 23:51:21 +0530

    Format code using 'cargo fmt'

--- filters/src/lib.rs
+++ filters/src/lib.rs
@@ -10,13 +10,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{
-	ItemFn,
-	ReturnType,
-	Type,
-	parse_macro_input,
-	parse_str,
-};
+use syn::{parse_macro_input, parse_str, ItemFn, ReturnType, Type};
 
 /** Produces a `#[calm_io::pipefail]` function attribute.
 
@@ -25,29 +19,29 @@ function which does not return `io::Result<_>`.
 **/
 #[proc_macro_attribute]
 pub fn pipefail(_attrs: TokenStream, item: TokenStream) -> TokenStream {
-	let mut func = parse_macro_input!(item as ItemFn);
-	//  TODO: Figure out how to extract the success type, then rewrite it to be
-	// `Result<Option<SUCCESS>, io::Error>`.
-	let ret_ty: Type = match ::std::mem::replace(
-		&mut func.sig.output,
-		parse_str("-> std::io::Result<()>").unwrap(),
-	) {
-		ReturnType::Default => parse_str("()").unwrap(),
-		ReturnType::Type(_, ty) => *ty,
-	};
-	let (attrs, vis, sig, block) = (func.attrs, func.vis, func.sig, func.block);
-	let out = quote! {
-		#( #attrs )*
-		#vis #sig {
-			let res: #ret_ty = (|| { #block })();
-			match res {
-				::std::result::Result::Err(e) => match e.kind() {
-					::std::io::ErrorKind::BrokenPipe => Ok(()),
-					_ => ::std::result::Result::Err(e),
-				},
-				::std::result::Result::Ok(_) => Ok(()),
-			}
-		}
-	};
-	out.into()
+    let mut func = parse_macro_input!(item as ItemFn);
+    //  TODO: Figure out how to extract the success type, then rewrite it to be
+    // `Result<Option<SUCCESS>, io::Error>`.
+    let ret_ty: Type = match ::std::mem::replace(
+        &mut func.sig.output,
+        parse_str("-> std::io::Result<()>").unwrap(),
+    ) {
+        ReturnType::Default => parse_str("()").unwrap(),
+        ReturnType::Type(_, ty) => *ty,
+    };
+    let (attrs, vis, sig, block) = (func.attrs, func.vis, func.sig, func.block);
+    let out = quote! {
+        #( #attrs )*
+        #vis #sig {
+            let res: #ret_ty = (|| { #block })();
+            match res {
+                ::std::result::Result::Err(e) => match e.kind() {
+                    ::std::io::ErrorKind::BrokenPipe => Ok(()),
+                    _ => ::std::result::Result::Err(e),
+                },
+                ::std::result::Result::Ok(_) => Ok(()),
+            }
+        }
+    };
+    out.into()
 }
