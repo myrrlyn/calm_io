27a99d6cfab61d295f48d4565d9f7c5b7bf6b412
Atul Bhosale <atul1bhosale@gmail.com>
2019-10-18 23:51:21 +0530

    Format code using 'cargo fmt'

--- examples/bad_yes.rs
+++ examples/bad_yes.rs
@@ -1,14 +1,11 @@
 //! Reimplementation of `yes(1)`, that panics on `SIGPIPE`.
 
 fn main() {
-	let mut text = std::env::args()
-		.skip(1)
-		.collect::<Vec<_>>()
-		.join(" ");
-	if text.trim().is_empty() {
-		text = "y".to_owned();
-	}
-	loop {
-		println!("{}", text);
-	}
+    let mut text = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+    if text.trim().is_empty() {
+        text = "y".to_owned();
+    }
+    loop {
+        println!("{}", text);
+    }
 }
